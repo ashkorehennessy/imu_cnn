af6cd9efc432aa5e4cd5bb68cecf12532635abcc
ashkorehennessy <ashkorehennessy@gmail.com>
2025-03-30 21:08:09 +0800

    imu_cnn: Convert NCNN model to C header file (ncnn2mem)

---
+++ main/imu_cnn.id.h
@@ -0,0 +1,21 @@
+#ifndef NCNN_INCLUDE_GUARD_imu_cnn_id_h
+#define NCNN_INCLUDE_GUARD_imu_cnn_id_h
+namespace imu_cnn_ncnn_param_id {
+const int LAYER_in0 = 0;
+const int BLOB_in0 = 0;
+const int LAYER_conv1drelu_0 = 1;
+const int BLOB_1 = 1;
+const int LAYER_maxpool1d_4 = 2;
+const int BLOB_2 = 2;
+const int LAYER_conv1drelu_1 = 3;
+const int BLOB_3 = 3;
+const int LAYER_maxpool1d_5 = 4;
+const int BLOB_4 = 4;
+const int LAYER_flatten_9 = 5;
+const int BLOB_5 = 5;
+const int LAYER_fcrelu_0 = 6;
+const int BLOB_6 = 6;
+const int LAYER_linear_3 = 7;
+const int BLOB_out0 = 7;
+} // namespace imu_cnn_ncnn_param_id
+#endif // NCNN_INCLUDE_GUARD_imu_cnn_id_h
